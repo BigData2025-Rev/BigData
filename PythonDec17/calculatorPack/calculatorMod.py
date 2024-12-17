0da48ebee137f059ae03df221246b9fd1343c4cd
WilliamTRev <william.terry@revature.com>
2024-12-17 11:57:50 -0500

    added calc example

---
+++ PythonDec17/calculatorPack/calculatorMod.py
@@ -0,0 +1,28 @@
+from calculator import Calculator
+from funexcept import CustomException
+import numbers
+
+
+class CalculatorImp(Calculator):
+    def addition(self, num1: int, num2: int)-> int:
+        if isinstance(num1,numbers.Number) and isinstance(num2, numbers.Number): #check to make sure input is numeric
+            result = num1 + num2
+            return result
+        else:
+            raise CustomException("One or more entries were not numeric")
+    
+    def subtraction(self, num1: int, num2: int)-> int:
+        if isinstance(num1,numbers.Number) and isinstance(num2, numbers.Number): #check to make sure input is numeric
+            result = num1 - num2
+            return result
+        else:
+            raise CustomException("One or more entries were not numeric")
+        
+    def rounding(self, result: float) -> int:
+        if isinstance(result, numbers.Number): #check to make sure input is numeric
+            return round(result)
+        else:
+            raise CustomException("Could not round the value")
+
+calc = CalculatorImp()
+print(calc.addition(5,5))
---
+++ PythonDec17/calculatorPack/testcalc.py
@@ -0,0 +1,55 @@
+import pytest
+from funexcept import CustomException
+from calculatorMod import CalculatorImp
+
+calculator = CalculatorImp()
+
+def test_addition_success():
+    result = calculator.addition(5,5)
+    assert result == 10 # we expect 5+5 to equal 10
+
+def subtraction_addition_success():
+    result = calculator.addition(5,5)
+    assert result == 10 # we expect 5+5 to equal 10
+
+def test_round_success():
+    result = calculator.rounding(1.1)
+    assert result == 1
+
+def test_addition_strings_entered():
+    with pytest.raises(CustomException):
+        result = calculator.addition("one", "two")
+
+def test_subtraction_strings_entered():
+    with pytest.raises(CustomException):
+        result = calculator.subtraction("one", "two")
+
+def test_round_string_entered():
+    with pytest.raises(CustomException):
+        result = calculator.rounding("five point seven")
+
+def test_addition_exception_message_correct():
+    try:
+        result = calculator.addition(1, "1")
+        assert False
+    except CustomException as e:
+        assert e.message == "One or more entries were not numeric"
+
+def test_subtraction_exception_message_correct():
+    try:
+        result = calculator.subtraction(1, "1")
+        assert False
+    except CustomException as e:
+        assert e.message == "One or more entries were not numeric"
+
+def test_round_exception_message_coorect():
+    try:
+        result = calculator.rounding("1.1")
+        assert False
+    except CustomException as e:
+        assert e.message == "Could not round the value"
+
+
+
+                                
+                                
