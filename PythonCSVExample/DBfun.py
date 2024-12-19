4d371f9ac4184bc91fc78344f324f29072e80247
WilliamTRev <william.terry@revature.com>
2024-12-19 09:26:39 -0500

    added DB sqllite practice

---
+++ PythonCSVExample/DBfun.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('students.db')
+
+c = conn.cursor()
+
+c.execute("""DROP TABLE IF EXISTS students""")
+c.execute("""CREATE TABLE students (name TEXT, age INTEGER)""")
+c.execute("""INSERT INTO STUDENTS VALUES ('Mark', 18)""")
+
+c.execute("""SELECT * FROM STUDENTS""")
+
+rows = c.fetchall()
+for row in rows:
+    print(row)
+
+conn.commit()
+
+conn.close()
