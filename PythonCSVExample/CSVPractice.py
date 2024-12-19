44d320fbaf19eda778077e800ee134228a4feecc
WilliamTRev <william.terry@revature.com>
2024-12-19 09:18:17 -0500

    added csv Practice

--- PythonCSVExample/CSVPractice.py
+++ PythonCSVExample/CSVPractice.py
@@ -14,5 +14,9 @@ Airports_Codes = [code for code, airport in rows]
 print(Airports_Codes[1:])
 file.close()
 
-
-
+file2 = open('C:/Users/WilliamTerry/Desktop/BigData2025GitHub/PythonCSVExample/airport_codes2.csv','w',newline="")
+csv_writer = csv.writer(file2)
+rowsNoHeader = rows[1:]
+for row in rowsNoHeader:
+    csv_writer.writerow(row)
+file2.close()
