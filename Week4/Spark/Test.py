00cb8df5a63ba6de7798399bc9af64c33d688050
WilliamTRev <william.terry@revature.com>
2025-01-08 09:41:58 -0500

    added Spark Test.py update

--- Week4/Spark/Test.py
+++ Week4/Spark/Test.py
@@ -8,4 +8,12 @@ rdd = sc.parallelize([1,2,3,4,5])
 
 print(rdd.collect())
 
+pokemon_rdd = sc.textFile("/user/will2/Pokemon.csv")
+
+pokemon_rdd_list = pokemon_rdd.map(lambda x: x.split(","))
+
+print(pokemon_rdd_list.take(5))
+
+pokemon_rdd_list.saveAsTextFile("/user/will2/pyspark_ex_out")
+
 sc.stop()
